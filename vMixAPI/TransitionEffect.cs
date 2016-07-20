0aaa21d9afa772fcb10c0110e62e0fcaee4947ed
elgarf <elgarf@XENA>
2016-07-20 15:57:03 +0500

    Updated transition effects

--- vMixAPI/TransitionEffect.cs
+++ vMixAPI/TransitionEffect.cs
@@ -8,19 +8,22 @@ namespace vMixAPI
 {
     public enum TransitionEffect
     {
-        Fade,
-        Zoom,
-        Wipe,
-        Slide,
-        Fly,
-        CrossZoom,
-        FlyRotate,
-        Cube,
-        CubeZoom,
-        VerticalWipe,
-        VerticalSlide,
-        Merge,
-        Stinger1,
-        Stinger2
+        Fade = 0,
+        Zoom = 1,
+        Wipe = 2,
+        Slide = 3,
+        Fly = 4,
+        CrossZoom = 5,
+        FlyRotate = 6,
+        Cube = 7,
+        CubeZoom = 8,
+        VerticalWipe = 20,
+        VerticalSlide = 25,
+        Merge = 30,
+        WipeReverse = 40,
+        SlideReverse = 42,
+        VerticalWipeReverse = 44,
+        VerticalSlideReverse = 46,
+        Cut = 100,
     }
 }
