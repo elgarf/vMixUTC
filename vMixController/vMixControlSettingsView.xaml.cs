6d920840174263f007a1c8c06b51d93de847b39c
elgarf <elgarf@outlook.com>
2018-08-26 17:26:15 +0500

    Minimize button disabled into widget properties window.

--- vMixController/vMixControlSettingsView.xaml.cs
+++ vMixController/vMixControlSettingsView.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using GalaSoft.MvvmLight.Ioc;
 using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Interop;
 
 namespace vMixController
 {
@@ -9,23 +12,40 @@ namespace vMixController
     /// </summary>
     public partial class vMixWidgetSettingsView : Window
     {
+        private const int GWL_STYLE = -16;
+        private const int WS_MINIMIZEBOX = 0x20000;
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
         /// <summary>
         /// Initializes a new instance of the vMixWidgetSettingsView class.
         /// </summary>
         public vMixWidgetSettingsView()
         {
+
             InitializeComponent();
             GalaSoft.MvvmLight.Messaging.Messenger.Default.Register<bool>(this, x =>
             {
                 DialogResult = x;
             });
             Closed += (o, e) => Messenger.Default.Unregister(this);
+
+            
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             /*e.Cancel = true;
             this.Hide();*/
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            long value = GetWindowLong(hwnd, GWL_STYLE);
+            SetWindowLong(hwnd, GWL_STYLE, (int)(value & ~WS_MINIMIZEBOX));
         }
     }
 }
