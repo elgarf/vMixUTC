581c0e14a763f22b007374d3f740fc67b7a3e80d
Смирнов Александр <elgarf@outlook.com>
2020-08-11 12:36:59 +0500

    Button widget UI fixes

--- vMixController/Converters/TextIsPathToImageConverter.cs
+++ vMixController/Converters/TextIsPathToImageConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommonServiceLocator;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -7,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
+using vMixController.ViewModel;
 
 namespace vMixController.Converters
 {
@@ -18,7 +20,10 @@ namespace vMixController.Converters
             var p = string.IsNullOrEmpty((string)parameter);
             if (value is string v)
             {
-                if (_extensions.Contains(Path.GetExtension(v)))
+                var relativePath = Classes.Utils.SearchFile(v, Directory.GetCurrentDirectory());
+                string foundPath = Classes.Utils.SearchFile(v, Path.GetDirectoryName(ServiceLocator.Current.GetInstance<MainViewModel>().ControllerPath));
+
+                if (_extensions.Contains(Path.GetExtension(v)) && (File.Exists(v) || File.Exists(relativePath) || File.Exists(foundPath)))
                     return p ? Visibility.Visible : Visibility.Collapsed;
             }
             return p ? Visibility.Collapsed : Visibility.Visible;
